681872d9d2728e66147459915bc8cb6dbb80af3b
Ade Syifa Maaniffiati <adsyfaa5325@gmail.com>
2023-07-06 12:20:51 +0700

    uas pemweb

--- application/views/form.blade.php
+++ application/views/form.blade.php
@@ -29,7 +29,8 @@
     <hr class="col-3 col-md-2 mb-5">
     
     <div class="row g-5">
-      <div class="col-md-6">
+      <div class="col-md-6"> 
+        <h2 class="text-body-emphasis">Form Update Artikel</h2>
         <h2 class="text-body-emphasis">Form Input Artikel</h2>
         <p>Masukan data yang akan di proses</p>
         <form method="post" action="{{ site_url('Welcome/simpan') }}">
